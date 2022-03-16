dd766ef846383ae763fadc6b04922e1f48c7b8a8
erickcirilo <39275273+erickcirilo@users.noreply.github.com>
2022-03-16 17:02:48 -0300

    modularizado el javascript

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
+import Persona from './persona.js';
+
 let myPerson = new Persona ('Juan', 'Perez', 23, 1.8);
 
 console.log (myPerson.saludar ());
+
+/* Tarea
+Crear una clase, animal, perro y Gato*/
